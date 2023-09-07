5c2c193ea5d3d753bf948acb23177b37ea9b5680
AndersonShen <shen.andersonshen@gmail.com>
2023-09-07 19:45:14 +0800

    update: 調整 vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/2023_web-layouting_week4/',
+  base: "/2023_web-layouting_week5/",
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }
+})
